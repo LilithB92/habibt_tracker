5f74295cd780c41d3856a5a53126bb5b676a6957
Lilith <bichakhchyanlilith1992@gmail.com>
2026-05-11 18:54:17 +0300

    model User added

--- users/models.py
+++ users/models.py
@@ -1,4 +1,28 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class User(AbstractUser):
-    pass
+    """
+      Stores a single user entry, related to :model:`users.Payment`
+      """
+
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Почта", help_text="Введите Вашу почту")
+    phone_number = models.CharField(
+        max_length=30, blank=True, null=True, verbose_name="Телефон", help_text="Введите Ваш номер телефона"
+    )
+    avatar = models.ImageField(upload_to="users/avatars/", blank=True, null=True, verbose_name="Аватарка")
+    country = models.CharField(
+        max_length=60, blank=True, null=True, verbose_name="Страна", help_text="Введите Ваша страна"
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
