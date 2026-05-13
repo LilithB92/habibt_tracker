d4bda3106b97fd200f6ba9e036fd15d6f914d64d
Lilith <bichakhchyanlilith1992@gmail.com>
2026-05-13 15:27:59 +0300

    The view  of public habits without the ability to edit or delete them.

--- habits/urls.py
+++ habits/urls.py
@@ -1,13 +1,19 @@
+from django.urls import path
 from rest_framework import routers
 
 from habits.apps import HabbitsConfig
 from habits.views import HabitViewSet
+from habits.views import PublicHabitViewSet
 
 app_name = HabbitsConfig.name
 
 router = routers.SimpleRouter()
 router.register(r"", HabitViewSet, basename="habit")
 
-urlpatterns = []
+
+urlpatterns = [
+    path('public_habit/', PublicHabitViewSet.as_view({'get': 'list'}), name="public_habit_list"),
+    path('public_habit/<int:pk>/', PublicHabitViewSet.as_view({'get': 'retrieve'}),  name="public_habit_retrieve"),
+]
 
 urlpatterns += router.urls
--- habits/views.py
+++ habits/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
 from .models import Habit
 from .serializers import HabitSerializer
@@ -11,9 +12,18 @@ class HabitViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         """Пользователь видит только свои привычки"""
-        return Habit.objects.filter(user=self.request.user)
+        return Habit.objects.filter(user=self.request.user).order_by('pk')
 
     def perform_create(self, serializer):
         """При создании привычки поле для владельца сущности заполняется аутентифицированным пользователем."""
         serializer.save(user=self.request.user)
-        serializer.save()
+
+
+class PublicHabitViewSet(viewsets.ReadOnlyModelViewSet):
+    """  Представление для публичных привычек, только для просмотра """
+    serializer_class = HabitSerializer
+    permission_classes = (AllowAny,)
+
+    def get_queryset(self):
+        """Пользователь видит только публичные привычки"""
+        return Habit.objects.filter(is_public=True).order_by('pk')
