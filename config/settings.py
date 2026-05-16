1c475961a494c52764067fd8691527213b8f01aa
Lilith <bichakhchyanlilith1992@gmail.com>
2026-05-16 23:37:19 +0300

    Set celery

--- config/settings.py
+++ config/settings.py
@@ -169,6 +169,19 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:8000",]
-CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000/",]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:8000/",
+]
 CORS_ALLOW_ALL_ORIGINS = False
+
+
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+# URL-адрес брокера сообщений
+CELERY_BROKER_URL = "redis://localhost:6379/0"  # Например, Redis, который по умолчанию работает на порту 6379
+# URL-адрес брокера результатов, также Redis
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
